a3f6fd6fdcb847e4e6313cd12fc94c9978ef292d
lekanda <abernaolao@gmail.com>
2020-05-14 14:29:24 +0200

    227

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,18 @@
 // Codigo para mostrar el mapa de Leaflet
+// GIST de Leaflet https://gist.github.com/Lekanda/1c39b4e5752395939f751d7b36777762
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
 const lat = 43.12717;
 const lng = -2.76615;
+const map = L.map('map').setView([lat, lng], 12);
+let marker; // Global por que se usa en varios
 
 
-const map = L.map('map').setView([lat, lng], 23);
+
 
 document.addEventListener('DOMContentLoaded', () => {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/    copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.google.com">Lekanda.NET</a> Contribuciones'
     }).addTo(map);
 
     // Buscar la Direccion
@@ -27,9 +30,17 @@ function buscarDireccion(e) {
         const provider = new OpenStreetMapProvider();
         // console.log(provider);
         provider.search({ query: e.target.value }).then((resultado) => {
-            // console.log(resultado);
+            console.log(resultado);
+            //Mostrar el mapa
+            map.setView(resultado[0].bounds[0], 15);
             // Agregar el Pin
-            
+            marker = new L.marker(resultado[0].bounds[0], {
+                draggable: true, // Para mover el PIN
+                autoPan: true // Para mover el mapa
+            })
+            .addTo(map)// Añade el PIN al Mapa
+            .bindPopup(resultado[0].label)
+            .openPopup()
         })
     }
 }
@@ -38,7 +49,3 @@ function buscarDireccion(e) {
 // L.marker([43.1272, -2.767]).addTo(map)
 //     .bindPopup('Iturriotz Barrena.<br> Dima Bizkaia.')
 //     .openPopup('prueba');
-
-L.marker([lat, lng]).addTo(map)
-    .bindPopup('Iturriotz Barrena.<br> Dima Bizkaia.')
-    .openPopup();

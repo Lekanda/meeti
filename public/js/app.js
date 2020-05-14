0328c7d26de9b07d4d6c3f98e231391419742550
lekanda <abernaolao@gmail.com>
2020-05-14 11:53:49 +0200

    226

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,44 @@
 // Codigo para mostrar el mapa de Leaflet
-var map = L.map('map').setView([43.1272, -2.767], 13);
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+const lat = 43.12717;
+const lng = -2.76615;
 
-L.marker([43.1272, -2.767]).addTo(map)
-    .bindPopup('Iturriotz Barrena.<br> Dima Bizkaia.')
-    .openPopup('prueba');
 
-L.marker([43.1692, -2.587]).addTo(map)
+const map = L.map('map').setView([lat, lng], 23);
+
+document.addEventListener('DOMContentLoaded', () => {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/    copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Buscar la Direccion
+    const buscador = document.querySelector('#formbuscador');
+    buscador.addEventListener('input', buscarDireccion);
+})
+
+
+function buscarDireccion(e) {
+    // console.log(e.target.value);
+    if(e.target.value.length > 8) {
+        // console.log('Buscando...');
+
+        // Utilizar el provider
+        const provider = new OpenStreetMapProvider();
+        // console.log(provider);
+        provider.search({ query: e.target.value }).then((resultado) => {
+            // console.log(resultado);
+            // Agregar el Pin
+            
+        })
+    }
+}
+
+
+// L.marker([43.1272, -2.767]).addTo(map)
+//     .bindPopup('Iturriotz Barrena.<br> Dima Bizkaia.')
+//     .openPopup('prueba');
+
+L.marker([lat, lng]).addTo(map)
     .bindPopup('Iturriotz Barrena.<br> Dima Bizkaia.')
     .openPopup();

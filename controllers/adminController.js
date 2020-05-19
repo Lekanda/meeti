4ddcfe4574ec31365b88cc4ab365ebd9cb6ae08f
lekanda <abernaolao@gmail.com>
2020-05-19 14:04:29 +0200

    234

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,10 +1,22 @@
 const Grupos = require('../models/Grupos');
+const Meeti = require('../models/Meeti');
 
 exports.panelAdministracion = async (req,res) => {
-    const grupos = await Grupos.findAll({where: { usuarioId : req.user.id }});
+
+
+    // Consultas
+    const consultas = [];
+    consultas.push(Grupos.findAll({where: { usuarioId : req.user.id }}) );
+    consultas.push(Meeti.findAll({where: { usuarioId : req.user.id }}) );
+
+
+    // Array destructuring
+    const [grupos, meeti] = await Promise.all(consultas);
+
 
     res.render('administracion', {
         nombrePagina: 'Panel de Administracion',
-        grupos
+        grupos,
+        meeti
     })
 }

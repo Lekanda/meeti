f054b4b04bb482d035ec73810a3d99af1c3d9e6a
lekanda <abernaolao@gmail.com>
2020-05-23 15:32:30 +0200

    240

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,48 @@
 // GIST de Leaflet https://gist.github.com/Lekanda/1c39b4e5752395939f751d7b36777762
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-const lat = 43.12717;
-const lng = -2.76615;
+// Obtener valores de la DB
+/**"43.2574172087374"    */
+
+const lat = document.querySelector('#lat').value || 43.2574172087374;
+const lng = document.querySelector('#lng').value || -2.766155062564687;
+const direccion = document.querySelector('#direccion').value || '';
 const map = L.map('map').setView([lat, lng], 12);
+
 let markers = new L.FeatureGroup().addTo(map);// Contenedor para todos los markers
 let marker; // Es el PIN .Global por que se usa en varios
 
+// Colocar el Pinen Edicion 
+if(lat && lng){
+    const geocodeService = L.esri.Geocoding.geocodeService();
+    // Agregar el Pin
+    marker = new L.marker([lat, lng], {
+        draggable: true, // Para mover el PIN
+        autoPan: true // Para mover el mapa
+    })
+    .addTo(map)// Añade el PIN al Mapa
+    .bindPopup(direccion)// Añadir Globo de Informacion
+    .openPopup();// Globo de Informacion
 
+    // Asignar al contenedor Markers(PINes)
+    markers.addLayer(marker);
+
+    // Detectar movimiento del Marker
+    marker.on('moveend', function(e) {// Coge la posicion final del PIN al moverlo
+        marker = e.target;
+        // console.log(marker.getLatLng());
+        const posicion = marker.getLatLng();
+        map.panTo(new L.LatLng(posicion.lat, posicion.lng));
+        // Centra el mapa al PIN  
+        // Reverse geocoding, cuando el usuario reubica el PIN
+        geocodeService.reverse().latlng(posicion, 15).run(function(error, result) {
+            // console.log(result);
+            llenarInputs(result);
+            // Asigna los valores al Popup del Marker
+            marker.bindPopup(result.address.LongLabel);
+        });
+    })
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {

38d3370b73cccc294363713c2269f3618c22852c
lekanda <abernaolao@gmail.com>
2020-05-24 16:37:10 +0200

    252

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,17 @@
-exports.home = (req,res) => {
+const Categorias = require('../models/Categorias');
+
+
+exports.home = async (req,res) => {
+
+    //Promise para consultas de buscador (Busca un grupo en tu ciudad ) en el home
+    const consultas = [];
+    consultas.push( Categorias.findAll({}) );
+    //Destructuring para extraer y pasar a la vista
+    const [ categorias ] = await Promise.all(consultas)
+
+
     res.render('home', {
-        nombrePagina:'Inicio'
+        nombrePagina:'Inicio',
+        categorias
     });
 };

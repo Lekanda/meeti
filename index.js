fe1d861790c1ab05ec396324b4ff81d97497f887
lekanda <abernaolao@gmail.com>
2020-05-24 14:17:49 +0200

    250

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.use(flash());
 
 /****Mi Middleware(usuario logeado, flash mss, fecha actual)****/
 app.use((req,res,next) => {
+    res.locals.usuario = { ...req.user } || null;
     res.locals.mensajes = req.flash();
     const fecha = new Date();
     res.locals.year = fecha.getFullYear();

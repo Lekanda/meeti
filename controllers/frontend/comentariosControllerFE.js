4f13ce4d34d498bb43151f9926a1166f390ff4af
lekanda <abernaolao@gmail.com>
2020-06-05 15:14:19 +0200

    275

--- controllers/frontend/comentariosControllerFE.js
+++ controllers/frontend/comentariosControllerFE.js
@@ -24,5 +24,27 @@ exports.agregarComentario = async ( req,res,next)=> {
 
 // Elimina un comentario de la DB
 exports.eliminarComentario = async(req,res,next) => {
-    res.send('Hola');
+    // console.log(req.body);
+
+    // Tomar el ID del comentario
+    const { comentarioId } = req.body;
+
+    // Consultar el comentario
+    const comentario = await Comentarios.findOne({ where: { id : comentarioId }});
+    console.log(comentario);
+    
+    // Verificar sí existe el comentario
+    if(!comentario) {
+        res.send('Accion no valida');
+        return next();
+    }
+
+    //Verificar que quien lo borra sea el creador
+    if(comentario.usuarioId === req.user.id) {
+        res.send(' Si es la persona correcta la que quiere borrar el comentario');
+        return next();
+    } else {
+        res.send('Te crees Hacker?');
+        return next();
+    }
 }
--- public/js/eliminarComentario.js
+++ public/js/eliminarComentario.js
@@ -14,10 +14,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function eliminarComentario(e) {
     e.preventDefault();
-    // console.log(this.action);
-    axios.post(this.action)
-        .then(respuesta => {
-            console.log(respuesta);
-        })
-    
+
+    Swal.fire({
+        title: 'Eliminar Comentario?',
+        text: "No se puede recuperar una vez borrado!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, Borralo!',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.value) {
+
+            // Tomar el id del comentario
+            const comentarioId = this.children[0].value;
+            console.log(comentarioId);
+
+            // Crear el objeto
+            const datos = {
+                comentarioId
+            }
+
+            // Ejecutar Axios y pasar los datos
+
+
+            axios.post(this.action, datos) // El 1º la URL; el 2º los datos que se pasan
+                .then((respuesta) => {
+                    console.log(respuesta);
+                });
+            Swal.fire(
+                'Borrado!',
+                'Tu comentario ha sido borrado.',
+                'success'
+            )
+    }
+    })
 }
